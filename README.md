024e87b53d9f1d9a8f5bb83ff9a1292c8d4a581f
jrave9000 <31921665+jrave9000@users.noreply.github.com>
2020-10-25 14:22:01 +0300

    mvp

---
+++ README.md
@@ -0,0 +1,3 @@
+# aws_create_vpc_link
+Ansible module to create Amazon AWS API Gateway VPC link.
+For now it doesn't check if the link already exists, **so the module is not idempotent**.
---
+++ aws_create_vpc_link.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: aws_create_vpc_link
+version_added: 0.1
+short_description: Create VPC link
+description:
+  - Create Amazon API Gateway VPC link
+requirements: [ boto3 ]
+author: "@jr9000"
+options:
+  name:
+    description:
+      - The name used to label and identify the VPC link.
+    required: true
+    type: str
+  target_arns:
+    description:
+      - The ARN of the network load balancer of the VPC targeted by the VPC link. The network load balancer must be owned by the same AWS account of the API owner.
+    required: true
+    type: list
+    elements: str
+  description:
+    description: The description of the VPC link.
+    type: str
+
+extends_documentation_fragment:
+- amazon.aws.aws
+- amazon.aws.ec2
+'''
+
+EXAMPLES = r'''
+# Note: These examples do not set authentication details, see the AWS Guide for details.
+- name: Create VPC Link
+  aws_create_vpc_link:
+    name: mylink
+    description: "some description"
+      target_arns:
+        - "arn:aws:elasticloadbalancing:region-code:account-id:loadbalancer/net/load-balancer-name/load-balancer-id"
+'''
+
+RETURN = r'''
+msg:
+  description: Message
+  returned: success
+  type: dict
+  sample: ""
+'''
+
+import json
+
+try:
+    import botocore
+except ImportError:
+    pass  # Handled by AnsibleAWSModule
+
+import traceback
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
+
+
+def main():
+    argument_spec = dict(
+        name=dict(type='str', required=True),
+	target_arns=dict(type='list', elements='str', required=True),
+	description=dict(type='str', required=False)
+    )
+
+
+    module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+    )
+
+    name = module.params.get('name')
+    target_arns = module.params.get('target_arns')
+    description = module.params.get('description')	
+
+
+    client = module.client('apigateway')
+
+    changed = True
+
+    msg = create_vpclink(client, name, target_arns, description)
+    exit_args = {}
+    exit_args['msg'] = camel_dict_to_snake_dict(msg)
+
+    exit_args['changed'] = changed
+
+    module.exit_json(**exit_args)
+
+retry_params = {"retries": 10, "delay": 10, "catch_extra_error_codes": ['TooManyRequestsException']}
+
+@AWSRetry.jittered_backoff(**retry_params)
+def create_vpclink(client, name,target_arns, description=None):
+    return client.create_vpc_link(name=name, description=description, targetArns=target_arns)
+
+
+if __name__ == '__main__':
+    main()
+
